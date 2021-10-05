6257e7edd953b596c1476b2fc1597532f42d9245
Igor Shestakov <jeygid@yandex.ru>
2021-10-06 00:39:58 +0300

    Added DB and forms

--- src/main/java/com/example/sweater/GreetingController.java
+++ src/main/java/com/example/sweater/GreetingController.java
@@ -1,14 +1,21 @@
 package com.example.sweater;
 
+import com.example.sweater.entity.Message;
+import com.example.sweater.repos.MessageRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
 
 import java.util.Map;
 
-
 @Controller
 public class GreetingController {
+
+    @Autowired
+    private MessageRepo messageRepo;
 
     @GetMapping("/greeting")
     public String greeting(
@@ -24,7 +31,33 @@ public class GreetingController {
 
     @GetMapping
     public String main(Map<String, Object> model) {
-        model.put("some", "Hello, user");
+        Iterable<Message> messages = messageRepo.findAll();
+        model.put("messages", messages);
+        return "main";
+    }
+
+    @PostMapping
+    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
+
+        Message message = new Message(tag, text);
+        messageRepo.save(message);
+
+        return "main";
+    }
+
+    @PostMapping("filter")
+    public String filter(@RequestParam String filter, Map<String, Object> model) {
+
+        Iterable<Message> messages;
+
+        if (!filter.isEmpty() && filter != null) {
+            messages = messageRepo.findByTag(filter);
+        } else {
+            messages = messageRepo.findAll();
+        }
+
+        model.put("messages", messages);
+
         return "main";
     }
 
